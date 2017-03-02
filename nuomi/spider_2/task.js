0c516a005de722064774f90e48d8a5e685ccc323
Mrdaigithub <346139312@qq.com>
2017-03-02 09:50:10 +0800

    completed spider_2

--- nuomi/spider_2/task.js
+++ nuomi/spider_2/task.js
@@ -2,48 +2,56 @@
 
 var page = require('webpage').create()
 var system = require('system')
+var devices = require('./devices.json')
 
-phantom.outputEncoding = 'gbk';
-page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
-page.settings.viewportSize = {
-    width: 320,
-    height: 568
-}
-
-
+var deviceName = system.args[2] || 'iPhone5'
+var theDevice = devices[deviceName]
 var startTime = (new Date()).getTime()
 var wd = system.args[1]
-var deviceName = system.args[2]
 var url = encodeURI('https://baidu.com/s?wd=' + wd)
 var data = {
-    code:1,
-    msg:'抓取成功',
-    word:system.args[1] || '',
-    time:0,
-    dataList:[]
+    code: 1,
+    msg: '抓取成功',
+    word: system.args[1] || '',
+    time: 0,
+    dataList: [],
+    device: deviceName
 }
 
-page.open(url,function (s) {
-    if (s === 'success'){
+phantom.outputEncoding = 'gbk';
+page.settings.userAgent = theDevice['userAgent']
+page.settings.viewportSize = {
+    width: theDevice['viewportSize'].split('*')[0],
+    height: theDevice['viewportSize'].split('*')[1]
+}
+page.clipRect = {
+    top: 0, left: 0,
+    width: theDevice['viewportSize'].split('*')[0],
+    height: theDevice['viewportSize'].split('*')[1]
+}
+
+
+page.open(url, function (s) {
+    if (s === 'success') {
         var dataList = page.evaluate(function () {
             var dataList = []
             $('.result').each(function (index) {
                 dataList[index] = {
-                    title:$(this).find('.t').text()||'',
-                    info:$(this).find('.c-abstract').text()||'',
-                    link:$(this).find('a').attr('href')||'',
-                    pic:$(this).find('.c-img').attr('src')||''
+                    title: $(this).find('.t').text() || $(this).find('.c-title').text() || '',
+                    info: $(this).find('.c-abstract').text() || $(this).find('.wa-bk-polysemy-clamp').text() || '',
+                    link: $(this).find('a').attr('href') || '',
+                    pic: $(this).find('img').attr('src') || ''
                 }
             })
             return dataList
         })
-    }else {
+    } else {
         data.code = 0
         data.msg = '抓取失败'
     }
-    page.render('search.jpg')
+    page.render('search.jpeg', {format: 'jpeg', qualiy: 100})
     data['dataList'] = dataList
     data.time = (new Date()).getTime() - startTime
-    console.log(JSON.stringify(data,null,4))
+    console.log(JSON.stringify(data, null, 4))
     phantom.exit()
 })
