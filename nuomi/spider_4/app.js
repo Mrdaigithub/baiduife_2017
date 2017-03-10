7671f6bc54358911487b644ce1eb64f20ae6e798
Mrdaigithub <346139312@qq.com>
2017-03-10 23:35:11 +0800

    update spider_4

--- nuomi/spider_4/app.js
+++ nuomi/spider_4/app.js
@@ -1,20 +1,35 @@
-"use strict";
+"use strict"
 
+const child_process = require('child_process')
 const Koa = require('koa')
-const router = require('koa-router')()
+const koaBody = require('koa-body')
 const app = new Koa()
 
-router.post('/', async (next)=>{
-    next()
-})
-app.use(async (ctx,next)=>{
-    console.log(ctx)
-    ctx.body = 'asd'
-})
+app.use(koaBody({formidable:{uploadDir: __dirname}}))
 
-app.use(router.routes())
+app.use(async (ctx,next)=>{
+    ctx.set({
+        'Access-Control-Allow-Origin': '*',
+        'Server': 'Koa2'
+    })
+    if (ctx.method === 'POST'){
+        ctx.status = 200
+        ctx.set('Content-Type', 'text/plain')
+        ctx.body = 'hello world!'
+        console.log(ctx.request.body)
+        // let command = `phantomjs task.js ${ctx.request.body.keyword} ${ctx.request.body.deviceName}`
+        // console.log(command)
+        // child_process.exec(command,(err, stdout, stderr)=>{
+        //     if (err) throw err
+        //     if (stderr) throw `stderr: ${stderr}`
+        //     console.log(`stdout: ${stdout}`)
+        // })
+    }else{
+        ctx.status = 500
+        ctx.set('Content-Type', 'text/plain')
+        ctx.body = 'Request error'
+    }
+})
 
 app.listen(8080)
-
-
 
--- nuomi/spider_4/index.js
+++ nuomi/spider_4/index.js
@@ -3,21 +3,19 @@
 const app = new Vue({
     data:{
         keyword: '',
-        deviceModel:'iPhone5',
+        deviceName:'iPhone5',
         res:''
     },
     methods:{
         submit(){
-            axios.post('http://127.0.0.1:8080/', {
-                keyword: this.keyword,
-                deviceModel: this.deviceModel
-            })
-                .then(function (response) {
-                    this.res = response
+            axios.post('http://127.0.0.1:8080/',JSON.stringify({keyword:this.keyword, deviceName:this.deviceName}))
+                .then(res=>{
+                    this.res = res.data
                 })
-                .catch(function (error) {
-                    console.error(error);
-                });
+                .catch(err=>{
+                    console.error(`err ${err}`)
+                })
+            this.keyword = ''
         }
     }
 }).$mount('#app')
