eb24fcf2490e471d666c4e5c53c972375150d6f1
Mrdaigithub <346139312@qq.com>
2017-04-03 22:59:33 +0800

    completed ECharts_3

--- ECharts&WebVR/ECharts_3/app.js
+++ ECharts&WebVR/ECharts_3/app.js
@@ -1,55 +1,33 @@
 "use strict";
 
 let fs = require('fs')
-const http = require('http');
+const koa = require('koa')
+const serve = require('koa-static');
+const views = require('koa-views');
+const csv = require('csvtojson')
+const app = new koa()
 
-const hostname = '0.0.0.0';
-const port = 3000;
-
-
-/**
- * 文件预处理
- * @param filename1
- * @param filename2
- * @param done
- */
-function pretreatment(filename1, filename2, done) {
-    let input = fs.createReadStream(filename1)
-    let output = fs.createWriteStream(filename2)
-    let res = []
-
-    input.on('data', function (d) {
-        res = JSON.stringify(
-            d
-            .toString()
-            .split(/\r|\n/).filter(e=>e)
-            .map(e=>e.split(','))
-        )
-        res = res.substring(0, res.length-1)
-        output.write(res)
-    })
-    input.on('error', function (err) {
-        throw err
-    })
-    input.on('end', function () {
-        output.end(']')
-        if (done) done()
-    });
-}
-
-fs.exists('./sp500hst.txt', exists => {
-    if (!exists) throw 'file not exists'
-    pretreatment('./sp500hst.txt', './sp500hst.json', () => {
-        const server = http.createServer((req, res) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/json');
-            fs.readFile('./sp500hst.json', (err, data)=>{
-                res.end(data);
-            })
-        });
-
-        server.listen(port, hostname, () => {
-            console.log(`Server running at http://${hostname}:${port}/`);
-        });
-    })
+app.use(serve('.'));
+app.use(views(__dirname))
+app.use(async function (ctx) {
+    await ctx.render('index.html')
 })
+
+
+let data = []
+let csvReadStream = fs.createReadStream('sp500hst.txt')
+
+csv()
+    .fromStream(csvReadStream)
+    .on('csv', (csvRow) => {
+        data.push(csvRow)
+    })
+    .on('done', (error) => {
+        if (error) throw error
+        fs.writeFile('sp500hst.json', JSON.stringify(data), err => {
+            if (err) throw err
+            console.log('It\'s saved!');
+            app.listen(8080);
+        })
+        console.log('end')
+    })
